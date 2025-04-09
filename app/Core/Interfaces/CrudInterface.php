94b62a7984bce656a16f32f18a93ddd007a7d880
quamilou <quamilougame@gmail.com>
2025-04-09 15:30:59 +0200

    Initial commit

---
+++ app/Core/Interfaces/CrudInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+interface CrudInterface {
+    public function getAll();
+    public function getById($id);
+    public function save();
+    public function update($id);
+    public function delete($id);
+}
