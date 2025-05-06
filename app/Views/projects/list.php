eecb814354556abc2adc4d39a397cd160be55651
quamilou <quamilougame@gmail.com>
2025-05-06 15:31:28 +0200

    Update list.php

---
+++ app/Views/projects/list.php
@@ -0,0 +1,34 @@
+
+<h1>Liste des projets</h1>
+
+<a href="index.php?controller=project&action=form">+ Nouveau projet</a>
+
+<table border="1" cellpadding="8" cellspacing="0">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Titre</th>
+            <th>Date de début</th>
+            <th>Date de fin</th>
+            <th>Actions</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php if (!empty($projects)): ?>
+            <?php foreach ($projects as $project): ?>
+                <tr>
+                    <td><?= htmlspecialchars($project['id']) ?></td>
+                    <td><?= htmlspecialchars($project['title']) ?></td>
+                    <td><?= htmlspecialchars($project['start_date']) ?></td>
+                    <td><?= htmlspecialchars($project['end_date']) ?></td>
+                    <td>
+                        <a href="index.php?controller=project&action=detail&id=<?= $project['id'] ?>">Voir</a>
+                        <!-- Pas encore d'édition ou suppression -->
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        <?php else: ?>
+            <tr><td colspan="5">Aucun projet trouvé.</td></tr>
+        <?php endif; ?>
+    </tbody>
+</table>
