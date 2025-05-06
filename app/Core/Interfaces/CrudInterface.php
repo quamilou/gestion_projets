c22c7160438b72dd4670f422be142f76cb813d5c
quamilou <quamilougame@gmail.com>
2025-05-06 21:46:48 +0200

    Update CrudInterface.php

--- app/Core/Interfaces/CrudInterface.php
+++ app/Core/Interfaces/CrudInterface.php
@@ -1,9 +1,14 @@
 <?php
 
 interface CrudInterface {
+
     public function getAll();
+
     public function getById($id);
+
     public function save();
+
     public function update($id);
+    
     public function delete($id);
 }
